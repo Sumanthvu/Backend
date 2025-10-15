d8723724c603589121ab01bf0c70838142a12e93
Sumanthvu <sumanthveslavath07@gmail.com>
2025-10-15 16:39:33 +0530

    Database connection

---
+++ src/index.js
@@ -0,0 +1,56 @@
+//Approach 1
+//Using try-catch async await ifi
+/*
+
+//require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv'
+dotenv.config({
+    path: './env'
+})
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+
+import express from 'express'
+const  app=express()
+
+( async () => {
+    try{
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      app.on("error",()=>{
+        console.log("Error: ",error);
+        throw error;
+      })
+      app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+      })
+    }
+    catch(error){
+        console.error("Error: ",error);
+        throw error
+    }
+} ) ()
+
+*/
+
+
+
+
+//Approach 2
+
+//We will connect the database in a different file inside the DB folder
+//export that and import in the index file
+
+// require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv'
+dotenv.config({
+    path: './env'
+})
+
+import connectDB from './db/index.js';
+import { request } from 'express';
+
+
+
+connectDB();
